a39ad00a07fe1d76eafc57bfd4c120bf8d28e550
Scott <sdallman@gmail.com>
2020-10-30 10:13:03 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,14 @@
 node default {
 }
+
 node 'puppet-master.us-central1-a.c.cloudmasters-iac.internal' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^bd/ {
+  include role::db_server
 }
