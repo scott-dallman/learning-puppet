f5031eb4150d299d7362e51240db90437e7b9638
Scott <sdallman@gmail.com>
2020-10-30 11:23:44 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.us-central1-a.c.cloudmasters-iac.internal:}
-  dockeragent::node {'db.us-central1-a.c.cloudmasters-iac.internal:}
+  dockeragent::node {'web.us-central1-a.c.cloudmasters-iac.internal':}
+  dockeragent::node {'db.us-central1-a.c.cloudmasters-iac.internal':}
 }
