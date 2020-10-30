b4bf5005b24f330c1fa97ebcc36d3f359ad4ab29
Scott <sdallman@gmail.com>
2020-10-30 11:12:35 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,9 @@ node default {
 
 node 'puppet-master.us-central1-a.c.cloudmasters-iac.internal' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => "Welcome to ${fqdn}\n",
 }
 
 node /^web/ {
